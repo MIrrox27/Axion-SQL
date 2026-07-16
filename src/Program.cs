749a58538d50e46ae27c37b5e1008cdccf08aed1
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-07-16 19:30:01 +0500

    сделан первый файл

--- src/Program.cs
+++ src/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// author github.com/MIrrox27/Axion-SQL
+
+
+using System;
+
+namespace AxionSQL 
+{
+  class Program
+  {
+     static void Main()
+    {
+      Console.WriteLine("Axion SQL");
+    }
+  }
+}
