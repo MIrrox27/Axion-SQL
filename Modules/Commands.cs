1988d867de28bbbfbf7b18208b8ff7587b328ea6
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-07-16 20:46:30 +0500

    сделаны базовые команды

--- Modules/Commands.cs
+++ Modules/Commands.cs
@@ -1,2 +1,17 @@
 // author github.com/MIrrox27/Axion-SQL
 // Modules/Commands.cs
+
+namespace AxionSQL.Modules;
+
+public enum CommandTokens
+{
+  NEW, // Создать новую строку/колонку
+  PUSH, // добавить (обновить) элемент, пример ADD COLUMN "column_name" ROW "index" ELEMENT "element"
+  GET, // получить элемент, пример ADD COLUMN "column_name" ROW "index" -> выведет значение элемента
+  
+  DELETE, // удалить элемент/строку/колонку, пример ADD COLUMN "column_name" ROW "index" -> удалит элемент
+
+  ROW, // ключевое слово, после идет значение 
+  COLUMN, // ключевое слово, после идет значение 
+  ELEMENT // ключевое слово, после идет значение 
+}
