a6863bf3b19ca772f6b4c6092f8bdede9d68df77
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-07-17 12:40:37 +0500

    написана логика (коммент) парсера

--- Services/Parser.cs
+++ Services/Parser.cs
@@ -1,2 +1,24 @@
 // author github.com/MIrrox27/Axion-SQL
 // Services/Parser.cs
+
+using System.Collections.Generic;
+using AxionSQL.Modules;
+namespace AxionSQL.Modules;
+
+public class Parser
+{
+  public static List<int> Parse(string _command)
+  {
+    /*
+      Чо мне надо?
+
+      строка - разбить по пробелам 
+
+    
+    */
+
+
+    
+  }
+
+}
