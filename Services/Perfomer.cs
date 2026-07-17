e70fa01bfa0dd26f50e34a5096d96fa13111b951
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-07-17 13:42:30 +0500

    сделана обработка команды NEW

--- Services/Perfomer.cs
+++ Services/Perfomer.cs
@@ -1,2 +1,53 @@
 // author github.com/MIrrox27/Axion-SQL
 // Services/Perfomer.cs
+
+using AxionSQL.Modules;
+using AxionSQL.Services;
+
+
+public class Perfomer
+{
+  public static void Perfom(List<string> _command)
+  {
+    for (int i = 0; i < _command.Count; i++)
+    {
+      switch (_command[i])
+      {
+        case "NEW":
+          {
+            i++;
+
+            if (_command[i] == "COLUMN")
+            {
+              foreach (var el in DataStore.DataBase)
+              {
+                el.Add("");
+              }
+              i++;
+              DataStore.DataBase[0][^1] = _command[i];
+            }
+
+            else if (_command[i] == "ROW")
+            {
+              int len = DataStore.DataBase.Count;
+              List<string> new_row = new();
+              for (int j = 0; j < len; j++) new_row.Add("");
+              DataStore.DataBase.Add(new_row);
+            }
+
+            break;
+          }
+
+
+
+
+      }
+
+
+
+
+    }    
+
+  }
+
+}
