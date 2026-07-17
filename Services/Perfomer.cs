7ade1a29906546e6ec3b542887f46936947dae71
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-07-17 14:20:03 +0500

    добавлена обработка команды PUSH

--- Services/Perfomer.cs
+++ Services/Perfomer.cs
@@ -9,6 +9,7 @@ public class Perfomer
 {
   public static void Perfom(List<string> _command)
   {
+
     for (int i = 0; i < _command.Count; i++)
     {
       switch (_command[i])
@@ -34,18 +35,46 @@ public class Perfomer
               for (int j = 0; j < len; j++) new_row.Add("");
               DataStore.DataBase.Add(new_row);
             }
+            break;
+          }
 
+        case "PUSH" :
+          {
+            i++;
+            
+            int col = 0;
+            int row = 1;
+
+            if (_command[i] == "ROW")
+            {
+              i++;
+              row = int.Parse(_command[i]);
+            }
+            else throw new Exception($"Unexpected command {_command[i]}, expected 'ROW'");
+            
+            i++;
+
+            if (_command[i] == "COLUMN")
+            {
+              i++;
+              col = int.Parse(_command[i]);
+            }
+            else throw new Exception($"Unexpected command {_command[i]}, expected 'COLUMN'");
+
+            i++;
+
+            if (_command[i] == "ELEMENT")
+            {
+              i++;
+              string element = _command[i];
+              DataStore.DataBase[row][col] = element;
+            }
+            else throw new Exception($"Unexpected command {_command[i]}, expected 'ELEMENT'");
             break;
           }
 
 
-
-
       }
-
-
-
-
     }    
 
   }
