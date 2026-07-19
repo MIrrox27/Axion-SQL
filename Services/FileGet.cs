2d88031a1a9f3f48f33b14ccb816197f53c5b859
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-07-19 21:17:28 +0500

    добавлены функции для импорта таблиц из файла json

--- Services/FileGet.cs
+++ Services/FileGet.cs
@@ -1,2 +1,34 @@
 // author github.com/MIrrox27/Axion-SQL
 // Services/FileGet.cs
+
+using System.IO;          
+using System.Text.Json; 
+
+using AxionSQL.Modules;  
+
+
+namespace AxionSQL.Services;
+
+class FileGet
+{
+  public static int TableImport(string path)
+  {
+    if (File.Exists(path))
+    {
+    string importTable = File.ReadAllText(path);
+      try
+      {
+        if (!string.IsNullOrEmpty(importTable) && !string.IsNullOrWhiteSpace(importTable))
+        DataStore.DataBase = JsonSerializer.Deserialize<List<List<string>>>(importTable);
+      }
+      catch (JsonException)
+      {
+        return 1;
+      }
+    }
+    return 0;
+
+  }
+
+
+}
