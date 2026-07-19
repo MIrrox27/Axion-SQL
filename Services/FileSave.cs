e7cb1a7655a6ca205b83b4eeae5583c21b327079
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-07-19 21:17:57 +0500

    добавлены функции для сохранения таблиц в формате json

--- Services/FileSave.cs
+++ Services/FileSave.cs
@@ -1,2 +1,40 @@
 // author github.com/MIrrox27/Axion-SQL
 // Services/FileSave.cs
+
+using System.IO;          
+using System.Text.Json; 
+
+using AxionSQL.Modules;
+
+
+namespace AxionSQL.Services;
+
+class FileSave
+{
+  public static int TableSave()
+  {
+    string pathToSave = DataStore.DataBasePath + DataStore.DataBaseName;
+    var options = new JsonSerializerOptions { WriteIndented = true };
+    string prettyJson = JsonSerializer.Serialize(DataStore.DataBase, options); 
+
+    File.WriteAllText(pathToSave, prettyJson);
+
+    return 0;
+  }
+
+  public static int TableExport(string path)
+  {
+    var options = new JsonSerializerOptions { WriteIndented = true };
+    string prettyJson = JsonSerializer.Serialize(DataStore.DataBase, options); 
+    File.WriteAllText(path, prettyJson);
+    return 0;
+  }
+
+  public static void TableNew()
+  {
+    TableSave();
+    DataStore.SetDefault();
+    DataStore.DataBase = new();
+  }
+
+}
