d2fd4532d3ce2f93aea73a181fd3620f62044272
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-07-21 20:15:26 +0500

    добавлены функции для доступа к дб через веб

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
 ﻿// author github.com/MIrrox27/Axion-SQL
 // Program.cs
 
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 using AxionSQL.Services;
 using AxionSQL.Modules;
@@ -9,23 +13,32 @@ namespace AxionSQL
 {
   class Program
   {
-     static void Main()
+    private static readonly object Locker = new();
+    public static string ServerAddr = "http://localhost:5000";
+     
+     
+     static void Main(string[] args)
     {
+
+      _ = Task.Run(() => StartWebServer(args));
+
       Console.WriteLine("\n\n -- Axion SQL");
+      Console.WriteLine($"Server started on {ServerAddr}. Commands must send to /db");
+      
       while (true)   
       {
         Console.WriteLine($"\n\n-- Database name: {DataStore.DataBaseName}");
         Console.WriteLine($"-- Database path: {DataStore.DataBasePath}\n");
 
         Console.Write("\n> ");
-        string command = Console.ReadLine();
-        if (command == "q") break;
-        Perfomer.Perfom(Parser.Parse(command));
+        string? input = Console.ReadLine();
+        if (input == "q" || input == "Q") break;
+        Perfomer.Perfom(Parser.Parse(input));
         Console.Write("\n\n\n");
         PrintTable(DataStore.DataBase);
       }
 
-    // test command NEW COLUMN A NEW COLUMN B NEW ROW NEW ROW NEW ROW PUSH ROW 1 COLUMN 0 ELEMENT 67 GET ROW 1 COLUMN 0
+    
     }
     static void PrintTable(List<List<string>> data)
     {
@@ -55,6 +68,20 @@ namespace AxionSQL
         string rowTemplate = string.Join(" | ", formats);
 
         Console.WriteLine(rowTemplate, row.ToArray());
+    }
+
+     public static string Perfom(List<string> _command)
+    {
+      lock (Locker)
+      {
+        return Perfomer.Perfom(_command);
+      }
+    } 
+
+
+    public static void StartWebServer(string[] args)
+    {
+      Server.StartWebServer(Locker, args);
     }
   }
 }
