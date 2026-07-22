a86eb7a81c8714a5c57da9af92f0a69d35da2116
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-07-22 16:01:07 +0500

    добавленн буфер для хранения бд

--- Modules/DataStore.cs
+++ Modules/DataStore.cs
@@ -2,18 +2,24 @@
 // Services/Database.cs
 
 using System.Collections.Generic;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.IO;
 
 namespace AxionSQL.Modules;
 
 public static class DataStore
 {
   public static List<List<string>> DataBase { get; set; } = new List<List<string>> ();
-  
   private static readonly string DataBasePathDefault  = @"./";
   private static readonly string DataBaseNameDefault = @"table.json";
   
   public static string DataBasePath { get; set; } = DataBasePathDefault;
   public static string DataBaseName { get; set; } = DataBaseNameDefault;
+
+  public static Dictionary<string, string> AliasTables { get; set; } = new();
+  
+  public static string CurrentTable = DataBaseName;
+  public static string CurrentPath = Path.Combine(DataBasePath, DataBaseName);
 
   public static void SetDefault()
   {
